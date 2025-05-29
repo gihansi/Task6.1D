80e4fe19a3a5d224bb2f18274b9b2781e2d3fb24
gihansi <s223984676@deakin.edu.au>
2025-05-29 14:22:09 +1000

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.android.volley:volley:1.2.1")
+
 }
